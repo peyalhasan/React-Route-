d74f3378dc0a8c35c1961609a7c47db143718ac1
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-03 13:09:56 +0600

    React Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import './App.css'
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router';
+import HomePage from './Components/HomePage.jsx';
+import AboutPage from './Components/AboutPage.jsx';
+import RootLayout from './Components/RootLayout.jsx';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      Component: RootLayout,
+      children: [
+        {
+          index: true,
+          Component: HomePage
+        },
+        {
+          path: '/home',
+          Component: HomePage
+        },
+        {
+          path: '/about',
+          element: <AboutPage></AboutPage>
+        }
+      ]
+    },
 
+  ]);
   return (
-    <></>
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,34 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import { createBrowserRouter } from 'react-router';
-import { RouterProvider } from 'react-router';
-import HomePage from './Components/HomePage.jsx';
-import AboutPage from './Components/AboutPage.jsx';
-import RootLayout from './Components/RootLayout.jsx';
-const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: RootLayout,
-    children: [
-      {
-        index: true,
-        Component: HomePage
-      },
-      {
-        path: '/home',
-        Component: HomePage
-      },
-      {
-        path: '/about',
-        element: <AboutPage></AboutPage>
-      }
-    ]
-  },
 
-]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <App></App>
   </StrictMode>,
 )
