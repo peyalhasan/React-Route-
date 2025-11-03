1ba15b32e0eb5ada6df8995765385612857f2dae
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-03 12:56:33 +0600

    React Router

---
+++ src/Components/AboutPage.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div className='flex justify-between' >
+            <div className="card card-sm bg-base-200 max-w-60 shadow">
+                <figure className="hover-gallery">
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-1.webp" />
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-2.webp" />
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-3.webp" />
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-4.webp" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title flex justify-between">
+                        daisyUI Hat
+                        <span className="font-normal">$25</span>
+                    </h2>
+                    <p>High Quality classic cap hat with stitch logo</p>
+                </div>
+            </div>
+            <div className="card card-sm bg-base-200 max-w-60 shadow">
+                <figure className="hover-gallery">
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-1.webp" />
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-2.webp" />
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-3.webp" />
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-4.webp" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title flex justify-between">
+                        daisyUI Hat
+                        <span className="font-normal">$25</span>
+                    </h2>
+                    <p>High Quality classic cap hat with stitch logo</p>
+                </div>
+            </div>
+            <div className="card card-sm bg-base-200 max-w-60 shadow">
+                <figure className="hover-gallery">
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-1.webp" />
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-2.webp" />
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-3.webp" />
+                    <img src="https://img.daisyui.com/images/stock/daisyui-hat-4.webp" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title flex justify-between">
+                        daisyUI Hat
+                        <span className="font-normal">$25</span>
+                    </h2>
+                    <p>High Quality classic cap hat with stitch logo</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AboutPage;
